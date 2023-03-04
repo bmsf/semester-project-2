e4c50212f0149dbc090ac7e059075931043df468
Bjørn-Magnus Fromreide <bmsf@Bjrn-Magnuss-MacBook-Pro.local>
2023-03-04 18:44:31 +0100

    adds own make bid modal component

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,8 +1,6 @@
 import { useEffect, useState, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
-import { XMarkIcon } from '@heroicons/react/24/outline';
-import { motion as m, AnimatePresence } from 'framer-motion';
 
 import {
 	ImageCarousel,
@@ -15,6 +13,7 @@ import {
 import FetchProduct from '../api/fetchProduct';
 import bid from '../api/bid';
 import placeholder from '../assets/placeholder-image.png';
+import MakeBidModal from '../components/layout/MakeBidModal';
 
 const Product = ({ profile, handleLogout }) => {
 	const { id } = useParams();
@@ -29,14 +28,6 @@ const Product = ({ profile, handleLogout }) => {
 	const handleClick = () => {
 		setOpenModal(!openModal);
 		setLoading(false);
-	};
-
-	const handleBackdropClick = (e) => {
-		// Check if the target element is the backdrop (i.e., has the 'backdrop' id)
-		if (e.target.id === 'backdrop') {
-			// Close the menu
-			setOpenModal(false);
-		}
 	};
 
 	useEffect(() => {
@@ -128,12 +119,22 @@ const Product = ({ profile, handleLogout }) => {
 							</p>
 						</div>
 						{profile ? (
-							<Button
-								children='Make bid'
-								textColor='White'
-								backgroundColor='black'
-								handleClick={handleClick}
-							/>
+							<>
+								<Button
+									children='Make bid'
+									textColor='White'
+									backgroundColor='black'
+									handleClick={handleClick}
+								/>
+								<MakeBidModal
+									open={openModal}
+									handleClose={() => setOpenModal(!openModal)}
+									highestBid={highestBid}
+									onChange={onChange}
+									onSubmit={onSubmit}
+									formData={formData}
+								/>
+							</>
 						) : (
 							<p className='text-black'>
 								<Link className='font-bold' to='/login'>
@@ -162,58 +163,14 @@ const Product = ({ profile, handleLogout }) => {
 							handleClick={handleClick}
 						/>
 					)}
-					<AnimatePresence>
-						{openModal && (
-							<>
-								<m.div
-									initial={{ opacity: 0 }}
-									animate={{ opacity: 1 }}
-									exit={{ opacity: 0 }}
-									transition={{ duration: 0.2, opacity: 1 }}
-									onClick={handleBackdropClick}
-									id='backdrop'
-									className='overflow-y-hidden backdrop-blur-md bg-[#BCBCBF] overflow-x-hidden fixed top-0 right-0 left-0 z-50 h-screen flex items-center justify-center w-screen'
-								>
-									<div className='bg-white flex flex-col items-center p-12 justify-center h-full md:h-1/3  w-full md:w-2/3 lg:w-2/4 xl:w-1/3 rounded'>
-										<div className='w-full flex flex-col gap-20 md:gap-10'>
-											<div className='flex justify-between w-full'>
-												<h2 className='self-center'>Make a bid</h2>
-
-												<div className='hover:bg-gray rounded-full p-1'>
-													<XMarkIcon
-														className='h-8 w-8 hover:bg-gray rounded-full cursor-pointer'
-														onClick={() => setOpenModal(!openModal)}
-													/>
-												</div>
-											</div>
-											<form className='flex flex-col gap-2' onSubmit={onSubmit}>
-												<div className='relative '>
-													<input
-														type='number'
-														id='amount'
-														placeholder={`Current highest bid: ${
-															highestBid ? highestBid.amount : 'No bids'
-														}`}
-														onChange={onChange}
-														value={formData.amount}
-														className='w-full border border-black rounded-lg p-2.5 text-black'
-														title='The bid needs to be a number without space or any signs'
-													/>
-												</div>
-
-												<button
-													className='rounded-md font-thin py-2 text-white bg-transparent text-lg border border-black bg-black w-full'
-													type='submit'
-												>
-													Place Bid
-												</button>
-											</form>
-										</div>
-									</div>
-								</m.div>
-							</>
-						)}
-					</AnimatePresence>
+					<MakeBidModal
+						open={openModal}
+						handleClose={() => setOpenModal(!openModal)}
+						highestBid={highestBid}
+						onChange={onChange}
+						onSubmit={onSubmit}
+						formData={formData}
+					/>
 				</div>
 			</div>
 			<div className='mb-20 md:m-0'>
